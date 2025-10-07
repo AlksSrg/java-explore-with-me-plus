701054339adff0096dd7cb9e1c70b5155cd78805
Антон Лучик <Intemic@yandex.ru>
2025-10-07 17:35:15 +0500

    Fix check style #2

--- main-service/src/main/java/ru/practicum/event/mappers/EventMapper.java
+++ main-service/src/main/java/ru/practicum/event/mappers/EventMapper.java
@@ -4,17 +4,16 @@ import ru.practicum.event.dto.EventFullDto;
 import ru.practicum.event.dto.EventShortDto;
 
 public class EventMapper {
-	public static EventShortDto toEventShortDto(EventFullDto eventFullDto) {
-		
-		return EventShortDto.builder()
-				.annotation(eventFullDto.getAnnotation())
-				.category(eventFullDto.getCategory())
-				.confirmedRequests(eventFullDto.getConfirmedRequests())
-				.eventDate(eventFullDto.getEventDate())
-				.initiator(eventFullDto.getInitiator())
-				.paid(eventFullDto.getPaid())
-				.title(eventFullDto.getTitle())
-				.views(eventFullDto.getViews())
-				.build();
-	}
+    public static EventShortDto toEventShortDto(EventFullDto eventFullDto) {
+        return EventShortDto.builder()
+                .annotation(eventFullDto.getAnnotation())
+                .category(eventFullDto.getCategory())
+                .confirmedRequests(eventFullDto.getConfirmedRequests())
+                .eventDate(eventFullDto.getEventDate())
+                .initiator(eventFullDto.getInitiator())
+                .paid(eventFullDto.getPaid())
+                .title(eventFullDto.getTitle())
+                .views(eventFullDto.getViews())
+                .build();
+    }
 }
