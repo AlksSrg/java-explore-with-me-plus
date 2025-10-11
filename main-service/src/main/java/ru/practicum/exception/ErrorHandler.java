ef2e182e674d525f2526a03658ef18ec7632993d
Антон Лучик <Intemic@yandex.ru>
2025-10-11 13:46:48 +0500

    Fix check style

--- main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,10 +1,8 @@
 package ru.practicum.exception;
 
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
