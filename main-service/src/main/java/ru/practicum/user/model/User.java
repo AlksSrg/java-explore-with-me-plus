931d634261d7e70a5ce03a234a0f0eb9269c1998
Антон Лучик <Intemic@yandex.ru>
2025-10-18 21:59:37 +0500

    Fix error in User model

--- main-service/src/main/java/ru/practicum/user/model/User.java
+++ main-service/src/main/java/ru/practicum/user/model/User.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String name;
     private String email;
 
